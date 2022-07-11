20ef14f8a04bb24a4ff9cc9f8b696321a490ab77
Mina Ghaly <minanabil@msn.com>
2022-07-11 22:16:04 +1000

    function to display temp by city added

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,44 @@
 var currentDate = moment().format('L');
-
 var dayNumber = moment().format("D");
-console.log(dayNumber);
+var x = "";
 
-
-
+//here we get the forecast label days from 1 to 5 and assign each a date
 for (var index = 0; index < daysForecast.children.length; index++) {
-    document.querySelector("#daysForecast").children[index].innerHTML = moment().format('L');   
+    // document.querySelector("#daysForecast").children[index].innerHTML = moment().format('L');   
+    document.querySelector("#daysForecast").children[index].innerHTML = moment().add(index+1, 'days').format('L');
 }
+
+//7981694ef9c97bcfcb67516f35667455
+//https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+//http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=7981694ef9c97bcfcb67516f35667455
+
+async function forecastDaily(apiKey,city,countryCode,limit){
+var url1 = "http://api.openweathermap.org/geo/1.0/direct?q="+city +","+countryCode+"&limit="+limit+"&appid="+apiKey;
+console.log(url1);
+var response1 =  await fetch(url1);
+var data1 = await response1.json();
+var longLat = "lat="+data1[0].lat+"&lon="+data1[0].lon;
+var url2 = "https://api.openweathermap.org/data/2.5/onecall?"+longLat+"&exclude={part}&appid="+apiKey;
+var response2  = await fetch(url2);
+var data2 = await response2.json();
+var temp = 
+}
+
+
+    forecastDaily("7981694ef9c97bcfcb67516f35667455","perth","036",1);
+var citySearch = document.querySelector("#citySearch");
+var citySelectedValue  = citySearch.addEventListener("click",function(event){
+    if (event.target.value == "Search"){
+        console.log(document.getElementById("cityInpt").value);
+        return document.getElementById("cityInpt").value;
+    }
+    else if (event.target.value){
+        console.log(event.target.value);
+       return event.target.value;
+    }
+
+});
+
+// https://api.openweathermap.org/data/3.0/onecall/timemachine?lat={lat}&lon={lon}&dt={time}&appid={API key}
+
